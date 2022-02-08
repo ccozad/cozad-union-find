1ad4f826a4a888bf9542e5c98d441d6b8acc17bd
ccozad <ccozad@gmail.com>
2022-02-07 23:15:36 -0800

    Command line parsing in main, example data files

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,22 @@
-mod union_find;
+use clap::Parser;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Name of the file with node names by index
+    #[clap(short, long, value_name = "FILE")]
+    nodes: String,
+
+    /// Name of the file with node connections by index
+    #[clap(short, long, value_name = "FILE")]
+    connections: String,
+
+}
 
 fn main() {
-    //let node = ufnode::Node::new(uuid: String::from("test"));
+    let args = Args::parse();
+
+    println!("Node File: {}!", args.nodes);
+    println!("Connections File: {}!", args.connections);
 }
