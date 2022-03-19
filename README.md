3f7948ff81f8c78782f20867478b44b1a47d57ac
ccozad <ccozad@gmail.com>
2022-03-18 21:57:31 -0700

    Documentation
    - Improve README
    - Add rust doc comments
    - Start building out lib for publishing

--- README.md
+++ README.md
@@ -5,12 +5,116 @@ A Rust implementation of the union-find disjoint set graph algorithm
 
 # Quick Start
 
+## Using the named node interfaces
+For relatively small networks you can simply interact with nodes by name.
+
+``` rust
+use union_find::client as ufclient;
+
+let mut client = ufclient::Client::new();
+
+client.add_node("A");
+client.add_node("B");
+client.add_node("C");
+client.add_node("D");
+client.add_node("E");
+client.add_node("F");
+client.add_node("G");
+client.add_node("H");
+client.add_node("I");
+client.add_node("J");
+
+
+client.connect_nodes("E", "D");
+client.connect_nodes("D", "I");
+client.connect_nodes("G", "F");
+client.connect_nodes("J", "E");
+client.connect_nodes("C", "B");
+client.connect_nodes("I", "J");
+client.connect_nodes("F", "A");
+client.connect_nodes("H", "B");
+client.connect_nodes("G", "B");
+client.connect_nodes("B", "A");
+client.connect_nodes("G", "H");
+
+println!("\nDisjoint sets found: {}", client.disjoint_set_count())
+```
+
+Output
+```
+Disjoint sets found: 2
+```
+
+## Using the bulk interfaces
+
+When you have a large volume of connections to process you can skip the lookups that occur with named nodes and use the bulk interfaces. The process involves giving a vector of node names and then specifying connections between nodes by index.
+
+``` rust
+use union_find::client as ufclient;
+use union_find::client::BulkConnection as ufconnection;
+
+let mut client = ufclient::Client::new();
+let nodes = vec![
+    String::from("A"), 
+    String::from("B"), 
+    String::from("C"),
+    String::from("D"),
+    String::from("E"),
+    String::from("F"), 
+    String::from("G"), 
+    String::from("H"), 
+    String::from("I"), 
+    String::from("J")
+];
+client.add_nodes_bulk(nodes);
+
+let connections = vec![
+    ufconnection { a: 4, b: 3 },
+    ufconnection { a: 3, b: 8 },
+    ufconnection { a: 6, b: 5 },
+    ufconnection { a: 9, b: 4 },
+    ufconnection { a: 2, b: 1 },
+    ufconnection { a: 8, b: 9 },
+    ufconnection { a: 5, b: 0 },
+    ufconnection { a: 7, b: 2 },
+    ufconnection { a: 6, b: 1 },
+    ufconnection { a: 1, b: 0 },
+    ufconnection{ a: 6, b: 7 }
+];
+client.connect_nodes_bulk(connections);
+
+println!("\nDisjoint sets found: {}", client.disjoint_set_count())
+```
+
+Output
+```
+Disjoint sets found: 2
+```
+
 ## Run as a CLI
+
 ```
 cargo build
 cd target/debug
 ./cozad-union-find -n ../../data/nodes_small.txt -c ../../data/connections_small.txt
 
+```
+
+Example Output
+```
+Node File: ../../data/nodes_small.txt
+Processing nodes file...
+Nodes file processed
+Bulk adding nodes...
+Nodes bulk added
+
+Connections File: ../../data/connections_small.txt
+Processing connections file...
+Connections file processed
+Bulk connecting nodes...
+Nodes bulk connected
+
+Disjoint sets found: 2
 ```
 
 ## Run the tests
--- src/union_find/client.rs
+++ src/union_find/client.rs
@@ -4,28 +4,44 @@ mod client_tests;
 
 use std::collections::HashMap;
 
+/// A node in the graph
 #[derive(Hash, Eq, PartialEq, Debug)]
 struct Node {
+    /// Unique ID of the node
     pub uuid: String,
+    /// Index for the node's parent
     pub parent_index: usize,
+    /// Index where the node is stored
     pub index: usize,
+    /// Number items in chain
     pub size: usize
 }
 
 #[derive(Hash, Eq, PartialEq, Debug)]
 pub struct BulkConnection {
+    /// Index of first connection
     pub a: usize,
+    /// Index of second connection
     pub b: usize
 }
 
 #[derive(Debug)]
 pub struct Client {
+    /// Storage for nodes in the graph
     nodes: Vec<Node>,
+    /// Map of names to index
     node_map: HashMap<String, usize>,
+    /// Number of disjoint sets
     set_count: usize
 }
 
 impl BulkConnection {
+    /// Constructs a new `BulkConnection`
+    /// 
+    /// # Arguments
+    ///
+    /// * `a` - Index of first connection
+    /// * `b` - Index of second connection
     pub fn new(a: usize, b: usize) -> Self {
         BulkConnection {
             a,
@@ -35,6 +51,7 @@ impl BulkConnection {
 }
 
 impl Client {
+    /// Constructs a new `Client`
     pub fn new() -> Self {
         let node_map = HashMap::new();
         let mut nodes = Vec::new();
@@ -54,6 +71,12 @@ impl Client {
         }
     }
 
+    /// Adds a node with given unique id
+    ///
+    /// # Arguments
+    ///
+    /// * `uuid` - Unique ID of node
+    /// 
     pub fn add_node(&mut self, uuid: &str) {
         if !self.node_exists(uuid) {
             let node = Node { 
@@ -68,6 +91,12 @@ impl Client {
         }
     }
 
+    /// Adds a multiple nodes with a single call
+    /// 
+    /// # Arguments
+    ///
+    /// * `uuid_list` - Collection of unique IDs
+    /// 
     pub fn add_nodes_bulk(&mut self, uuid_list: Vec<String>) {
         for uuid in uuid_list.iter() {
             let node = Node { 
@@ -82,6 +111,13 @@ impl Client {
         }
     }
 
+    /// Connects two nodes using their unique id
+    /// 
+    /// # Arguments
+    ///
+    /// * `uuid_a` - Unique id first node
+    /// * `uuid_b` - Unique id second node
+    /// 
     pub fn connect_nodes(&mut self, uuid_a: &str, uuid_b: &str) {
         let uuid_a_root = self.find_root_index(uuid_a);
         let uuid_b_root = self.find_root_index(uuid_b);
@@ -103,6 +139,12 @@ impl Client {
         }
     }
 
+    /// Connects a collection of nodes using node indexes to avoid node lookups by name
+    ///
+    /// # Arguments
+    ///
+    /// * `connections` - Collection of graph connections
+    /// 
     pub fn connect_nodes_bulk(&mut self, connections: Vec<BulkConnection>) {
         for connection in connections.iter() {
             let uuid_a_root = self.find_root_index_bulk(connection.a + 1);
@@ -126,10 +168,17 @@ impl Client {
         }
     }
 
+    /// The number of sets that share no connection with another set
     pub fn disjoint_set_count(&self) -> usize {
         self.set_count
     }
 
+    /// Finds the connected node with no parent
+    /// 
+    /// # Arguments
+    ///
+    /// * `uuid` - Unique ID of node to find root of
+    /// 
     pub fn find_root_index(&self, uuid: &str) -> usize {
         let node_index = self.node_index(uuid);
         if node_index > 0 {
@@ -143,6 +192,12 @@ impl Client {
         }
     }
 
+    /// Finds the connected node with no parent, optimized to reduce lookups
+    /// 
+    /// # Arguments
+    ///
+    /// * `node_index` - Index of node to find the root of
+    /// 
     pub fn find_root_index_bulk(&self, node_index: usize) -> usize {
         let mut node = self.nodes.get(node_index).unwrap();
         while node.parent_index != node.index {
@@ -151,6 +206,13 @@ impl Client {
         node.parent_index
     }
 
+    /// Determines if two nodes are connected through any path
+    /// 
+    /// # Arguments
+    ///
+    /// * `uuid_a` - Unique ID of first connection
+    /// * `uuid_b` - Unique ID of second connection
+    /// 
     pub fn nodes_connected(&self, uuid_a: &str, uuid_b: &str) -> bool {
         let uuid_a_root = self.find_root_index(uuid_a);
         let uuid_b_root = self.find_root_index(uuid_b);
@@ -158,15 +220,28 @@ impl Client {
         uuid_a_root > 0 && uuid_a_root == uuid_b_root
     }
 
+    /// The number of unique nodes in the graph
     pub fn node_count(&self) -> usize {
         self.nodes.len() - 1
     }
 
+    /// Determines if a node exists by the given name
+    /// 
+    /// # Arguments
+    ///
+    /// * `uuid` - Unique ID of node
+    /// 
     pub fn node_exists(&self, uuid: &str) -> bool {
         let node_uuid = String::from(uuid);
         self.node_map.contains_key(&node_uuid)
     }
 
+    /// Gets the index for a node with a given unique ID
+    /// 
+    /// # Arguments
+    ///
+    /// * `uuid` - Unique ID of node
+    /// 
     pub fn node_index(&self, uuid: &str) -> usize {
         let node_uuid = String::from(uuid);
         if self.node_map.contains_key(&node_uuid) {
